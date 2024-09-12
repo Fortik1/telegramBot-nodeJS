5b20cd13d6c034be79cf7f4c50cfa03fa39d7140
Fortik1 <orlov121.121@mail.ru>
2024-09-12 20:25:46 +0300

    add .eslintignore

---
+++ src/scripts/parserScheduleHTML.js
@@ -0,0 +1,46 @@
+import puppeteer from "puppeteer";
+import * as cheerio from "cheerio";
+import { parseFromString } from 'dom-parser';
+
+const parserHTML = async () => {
+    const link = 'https://schedule.mstimetables.ru/publications/cdb2a14c-a891-4f9f-b56c-7e8eb559c766#/groups';
+
+    const browser = await puppeteer.launch({
+        executablePath: '/usr/bin/chromium-browser'
+    });
+    const page = await browser.newPage();
+
+    await page.goto(link, {
+        waitUntil: "networkidle2",
+    });
+
+    await page.waitForSelector('body');
+
+    const content = await page.content();
+
+    const $ = cheerio.load(content);
+
+    const elementsArray = [];
+
+    $('.d-flex.flex-column').each( (index, element ) => {
+        parseFromString($(element).html())
+            .getElementsByClassName('link')
+            .map((el) => elementsArray.push(el));
+    });
+
+    const normalizeArray = elementsArray.map((element) => {
+        const linkSchedule = link + element.attributes[1].value.split('groups')[1];
+        const nameGroup = element.childNodes[0].text;
+        return {
+            nameGroup,
+            linkSchedule,
+        }
+    });
+
+    console.log(normalizeArray);
+
+
+    await browser.close();
+};
+
+export default parserHTML;
