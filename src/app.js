07a784c3e85a304d0ac8809e7a09d69195fb361e
Fortik1 <orlov121.121@mail.ru>
2024-09-15 02:59:04 +0300

    mod app.js

--- src/app.js
+++ src/app.js
@@ -1,5 +1,7 @@
-import parser from './scripts/parserScheduleHTML.js'
+import parser from './scripts/parserScheduleHTML.js';
+import parserByRef from './scripts/parseScheduleByRef.js';
 
 export default async () => {
    const normalizeDataSheduleAll = await parser();
+   console.log(await parserByRef());
 };
