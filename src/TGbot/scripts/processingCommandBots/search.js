361aad8bf53149132f872cfdc5e8b349889afb3e
Fortik1 <orlov121.121@mail.ru>
2024-09-15 20:30:26 +0300

    add search func

---
+++ src/TGbot/scripts/processingCommandBots/search.js
@@ -0,0 +1,22 @@
+import getDataBD from "../func/getDataBD.js";
+import {searchResult} from "../../answerText.js";
+
+export default (bot) => async (msg) => {
+    try {
+        const groupList = getDataBD('group_list');
+        const searchGroup = groupList.reduce((acc, { name_group }) => {
+            if (name_group.includes(msg.text.split('группа')[1].trim())) {
+                acc.push([{ text: name_group, callback_data: name_group }]);
+            }
+            return acc;
+        }, []);
+
+        await bot.sendMessage(msg.chat.id, searchResult, {
+            reply_markup: {
+                inline_keyboard: searchGroup
+            }
+        });
+    } catch (e) {
+        console.log(e);
+    }
+};
