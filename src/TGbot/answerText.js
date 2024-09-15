c608cc7fa58f0bb448274452a73afe6e28d73e20
Fortik1 <orlov121.121@mail.ru>
2024-09-15 20:31:50 +0300

    add new answerText

--- src/TGbot/answerText.js
+++ src/TGbot/answerText.js
@@ -1,9 +1,15 @@
 const reRegistration = 'Вы уже зарегистрированы) Если вы хотите сменить номер группы, нажмите /set_group';
 const startBot = 'Привет! Я бот который поможет тебе всегда знать своё расписание)';
-const newUser = 'Я вижу ты тут впервые. Тогда выбирай свою группу) \nТы можешь выбрать группу из списка ниже\nили \nвывести весь список /all_list_group';
+const newUser = `Я вижу ты тут впервые. Тогда выбирай свою группу) \nВыбирай способ поиска своей группы 👇🏻👇🏻👇🏻`;
+const search = `Напиши свою группу\n\n"группа номер группы" \n(к примеру: "группа ИСИП" или "группа 3101")`;
+const searchResult = `Выбирай свою группу 👇🏻👇🏻👇🏻`;
+const getAllGroup = ``;
 
 export {
     reRegistration,
     startBot,
-    newUser
+    newUser,
+    search,
+    searchResult,
+    getAllGroup,
 };
