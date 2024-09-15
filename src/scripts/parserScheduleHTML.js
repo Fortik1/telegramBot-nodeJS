1528adaa611de31e04319215d48b129b99892360
Fortik1 <orlov121.121@mail.ru>
2024-09-15 03:02:39 +0300

    вынес логику получения контена со страницы в отдельный файл, для исключения дублирования

--- src/scripts/parserScheduleHTML.js
+++ src/scripts/parserScheduleHTML.js
@@ -1,43 +1,33 @@
-import puppeteer from "puppeteer";
-import * as cheerio from "cheerio";
 import { parseFromString } from 'dom-parser';
+import * as cheerio from "cheerio";
+import getCurrentDate from "./getCurrentDate.js";
+import getContentByRef from "./getContentByRef.js";
 
 const parserScheduleHTML = async () => {
     const link = 'https://schedule.mstimetables.ru/publications/cdb2a14c-a891-4f9f-b56c-7e8eb559c766#/groups';
 
-    const browser = await puppeteer.launch({
-        executablePath: '/usr/bin/chromium-browser'
-    });
-    const page = await browser.newPage();
-
-    await page.goto(link, {
-        waitUntil: "networkidle2",
-    });
-
-    await page.waitForSelector('body');
-
-    const content = await page.content();
+    const content = await getContentByRef(link);
 
     const $ = cheerio.load(content);
 
     const elementsArray = [];
 
-    $('.d-flex.flex-column').each( (index, element ) => {
+    $('.d-flex.flex-column').each( (_index, element ) => {
         parseFromString($(element).html())
             .getElementsByClassName('link')
             .map((el) => elementsArray.push(el));
     });
 
+    const weekDate = getCurrentDate();
+
     const normalizeArray = elementsArray.map((element) => {
-        const linkSchedule = link + element.attributes[1].value.split('groups')[1];
+        const linkSchedule = link + element.attributes[1].value.split('groups')[1] + '?date=' + weekDate;
         const nameGroup = element.childNodes[0].text;
         return {
             nameGroup,
             linkSchedule,
         }
     });
-
-    await browser.close();
 
     return normalizeArray;
 };
