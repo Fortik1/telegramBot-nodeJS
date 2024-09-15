32bd42bc17fe7be8bed0a9f7e2431a5a2d12ba28
Fortik1 <orlov121.121@mail.ru>
2024-09-15 03:01:23 +0300

    add function --> get current date (дату следующей недели если сегодня вс или дату начала текущей недели для более корректных ссылок на расписание)

--- src/scripts/getCurrentDate.js
+++ src/scripts/getCurrentDate.js
@@ -1,8 +1,17 @@
 export default () => {
     const today = new Date();
-    const nextMonday = new Date(
-        today.getTime() + (1 + (1 - today.getDay() + 7) % 7) * 86400000
-    );
+    let nextMonday;
+    if (today.getDay() === 0) {
+        nextMonday = new Date(
+            today.getTime() + (1 + (1 - today.getDay() + 7) % 7) * 86400000
+        );
+    } else {
+        nextMonday = new Date(
+            today.getFullYear(),
+            today.getMonth(),
+            today.getDay() - today.getDay() + 1
+        );
+    }
 
     return nextMonday.toISOString().slice(0, 10);
 };
